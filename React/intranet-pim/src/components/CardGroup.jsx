7c9deee65a6fbbd823d70acbe0f36741067b1fcf
DashHunt <arthurlopescoutinho@hotmail.com>
2022-11-21 23:54:38 -0300

    Intranet Tool - DEV: Waiting for APIs

--- React/intranet-pim/src/components/CardGroup.jsx
+++ React/intranet-pim/src/components/CardGroup.jsx
@@ -1,15 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 
 import ConsultarCliente from "../assets/icons8-database-daily-import-100.png";
 import CadastrarCliente from "../assets/icons8-adicionar-grupo-de-usuários-mulher-homem-100.png";
 import CadastrarProposta from "../assets/icons8-adicionar-propriedade-100.png";
+import { routes } from "../helpers/helpers";
 
 function CardGroupComponent(props) {
+  const navigate = useNavigate()
+
+
+  function navigateTo(path){
+    navigate(path)
+  }
+
+
   return (
     <div>
       <CardGroup>
-        <Card className="m-3 rounded border border-light hover" style={{ cursor: "pointer" }}>
+        <Card className="m-3 rounded border border-light hover" style={{ cursor: "pointer" }} onClick={() => navigateTo(routes.clienteAll.path)}>
           <Card.Img
             variant="top"
             src={ConsultarCliente}
@@ -25,7 +36,7 @@ function CardGroupComponent(props) {
           </Card.Body>
           <Card.Footer className="bg-warning"></Card.Footer>
         </Card>
-        <Card className="m-3 rounded border border-light" style={{ cursor: "pointer" }}>
+        <Card className="m-3 rounded border border-light" style={{ cursor: "pointer" }} onClick={() => navigateTo(routes.cliente.path)}>
           <Card.Img
             variant="top"
             src={CadastrarCliente}
@@ -41,7 +52,7 @@ function CardGroupComponent(props) {
           </Card.Body>
           <Card.Footer className="bg-warning"></Card.Footer>
         </Card>
-        <Card className="m-3 rounded border border-light" style={{ cursor: "pointer" }}>
+        <Card className="m-3 rounded border border-light" style={{ cursor: "pointer" }} onClick={() => navigateTo(routes.solicitacao.path)}>
           <Card.Img
             variant="top"
             src={CadastrarProposta}
